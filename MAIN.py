ff62081d0ed8c08d7c47de99c9b91111b07896d9
Sanay711 <sanaytraining@gmail.com>
2025-08-06 16:33:56 +0530

    Add files via upload

---
+++ MAIN.py
@@ -0,0 +1,11 @@
+medical_cause=input("Did you have a medical cause 🚑? Yes or No:")
+
+atten = int(input("Enter the attendance of the student: "))
+
+if medical_cause == 'Yes':
+    print("You are allowed")
+else:
+    if atten>=75:
+        print("Allowed")
+    else:
+        print("Not allowed")
